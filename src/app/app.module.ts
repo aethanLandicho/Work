ae33aa5ccf1f5822d839eff335eb4760e29ee303
aethanLandicho <102212234+aethanLandicho@users.noreply.github.com>
2023-12-11 14:39:41 +0800

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IonicModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
